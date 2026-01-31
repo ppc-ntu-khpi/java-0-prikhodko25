8102078610480b9a78f1ad7a0fdcf4ac962e7c8f
Приходько Софія <sofiykapiryet@gmail.com>
2026-01-31 17:53:22 +0200

    Update Shirt.java

--- src/Shirt.java
+++ src/Shirt.java
@@ -1,9 +1,9 @@
 public class Shirt {
-  public int shirtID = 101; 
-  public String description = "My Custom Shirt"; 
-  public char colorCode = 'U';
-  public double price = 25.5; 
-  public int quantityInStock = 0; 
+  public int shirtID = 56; 
+  public String description = "My Shirt"; 
+  public char colorCode = 'R';
+  public double price = 100; 
+  public int quantityInStock = 34; 
   public void displayShirtInformation() {
     System.out.println("Shirt ID: " + shirtID);
     System.out.println("Shirt description:" + description);
