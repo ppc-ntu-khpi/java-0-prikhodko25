0aed6ad69386981c9c6d7fbcc2dc6d98e9cf9b2d
Приходько Софія <sofiykapiryet@gmail.com>
2026-01-31 18:04:16 +0200

    Add files via upload

---
+++ src/Shirt.java
@@ -0,0 +1,14 @@
+public class Shirt {
+  public int shirtID = 56; 
+  public String description = "My Shirt"; 
+  public char colorCode = 'R';
+  public double price = 100; 
+  public int quantityInStock = 34; 
+  public void displayShirtInformation() {
+    System.out.println("Shirt ID: " + shirtID);
+    System.out.println("Shirt description:" + description);
+    System.out.println("Color Code: " + colorCode);
+    System.out.println("Shirt price: $" + price);
+    System.out.println("Quantity in stock: " + quantityInStock);
+  } 
+} 
