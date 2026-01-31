4af5b7605480ace457087956a801ce39916784ce
Приходько Софія <sofiykapiryet@gmail.com>
2026-01-31 17:44:20 +0200

    Add files via upload

---
+++ src/Quotation.java
@@ -0,0 +1,7 @@
+public class Quotation {
+  String quote = "На світі той наймудріший, хто найдужче любить життя.";
+  String author = "Василь Симоненко";
+  public void display() {
+    System.out.println(quote + "\n- " + author);
+  }
+}
