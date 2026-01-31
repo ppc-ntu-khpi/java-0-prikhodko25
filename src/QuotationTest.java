2f6c267f755ac24ebfe39c4ab058c37f64a747e5
Приходько Софія <sofiykapiryet@gmail.com>
2026-01-31 17:40:19 +0200

    Add files via upload

--- src/QuotationTest.java
+++ src/QuotationTest.java
@@ -1,8 +1,6 @@
-public class Quotation {
-  String quote = "Борітеся – поборете, Вам Бог помагає!";
-  String author = "Тарас Шевченко";
-  public void display() {
-    System.out.println(quote + "\n- " + author);
-  }
+public class QuotationTest {
+    public static void main(String args[]) {
+        Quotation myQuotation = new Quotation();
+        myQuotation.display();
+    }
 }
-
